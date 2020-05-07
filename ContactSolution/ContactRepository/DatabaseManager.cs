a9d9ec743730a639f25365dce389151ae2a42855
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2020-05-07 11:46:28 -0700

    Added DBContext and Repository

---
+++ ContactSolution/ContactRepository/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ContactDB;
+
+namespace ContactRepository
+{
+    public class DatabaseManager
+    {
+        private static readonly ContactsContext _entities;
+         static DatabaseManager()
+        {
+            _entities = new ContactsContext();
+        }
+        // return an accessor to the database
+        public static ContactsContext Instance
+        {
+            get { return _entities; }
+        }
+    }
+}
