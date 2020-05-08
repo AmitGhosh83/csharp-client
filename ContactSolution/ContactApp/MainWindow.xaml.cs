959d91b0b3822458a3dace6d0add7129babfa3a8
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2020-05-07 19:48:00 -0700

    Updated the ListView on MainWindow

--- ContactSolution/ContactApp/MainWindow.xaml.cs
+++ ContactSolution/ContactApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,9 +24,58 @@ namespace ContactApp
         public MainWindow()
         {
             InitializeComponent();
+            LoadContacts();
+        }
+
+        private void LoadContacts()
+        {
+            var contacts = App.ContactRepository.GetAll();
+
+            uxContactList.ItemsSource = contacts
+                .Select(t => ContactModel.ToModel(t))
+                .ToList();
+
+            // OR
+            //var uiContactModelList = new List<ContactModel>();
+            //foreach (var repositoryContactModel in contacts)
+            //{
+            //    This is the .Select(t => ... )
+            //    var uiContactModel = ContactModel.ToModel(repositoryContactModel);
+            //
+            //    uiContactModelList.Add(uiContactModel);
+            //}
+
+            //uxContactList.ItemsSource = uiContactModelList;
         }
 
         private void OnNew_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ContactWindow();
+
+            if (window.ShowDialog() == true)
+            {
+                var uiContactModel = window.Contact;
+
+                var repositoryContactModel = uiContactModel.ToRepositoryModel();
+
+                App.ContactRepository.Add(repositoryContactModel);
+
+                // OR
+                //App.ContactRepository.Add(window.Contact.ToRepositoryModel());
+            }
+        }
+
+        private void uxFileNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void uxFileChange_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void uxFileDelete_Click(object sender, RoutedEventArgs e)
         {
 
         }
--- ContactSolution/ContactRepository/ContactRepository.cs
+++ ContactSolution/ContactRepository/ContactRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ContactDB;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ContactRepository
@@ -10,7 +11,7 @@ namespace ContactRepository
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Phonetype { get; set; }
+        public string PhoneType { get; set; }
         public string PhoneNumber { get; set; }
         public int Age { get; set; }
         public string Notes { get; set; }
@@ -27,7 +28,7 @@ namespace ContactRepository
                 ContactId = contactModel.Id,
                 ContactName = contactModel.Name,
                 ContactEmail = contactModel.Email,
-                ContactPhoneType = contactModel.Phonetype,
+                ContactPhoneType = contactModel.PhoneType,
                 ContactPhoneNumber = contactModel.PhoneNumber,
                 ContactAge= contactModel.Age,
                 ContactNotes= contactModel.Notes,
@@ -51,10 +52,63 @@ namespace ContactRepository
                 Name = dbContactModel.ContactName,
                 Notes = dbContactModel.ContactNotes,
                 PhoneNumber = dbContactModel.ContactPhoneNumber,
-                Phonetype = dbContactModel.ContactPhoneType
+                PhoneType = dbContactModel.ContactPhoneType
             };
             return contactModel;
         }
+        //Get the list of all contacts in Database and map it to ContactModel
+        public List<ContactModel> GetAll()
+        {
+            var items = DatabaseManager.Instance.Contact
+                .Select(x => new ContactModel
+                {
+                    Age = x.ContactAge,
+                    CreatedDate = x.ContactCreatedDate,
+                    Email = x.ContactEmail,
+                    Id = x.ContactId,
+                    Name = x.ContactName,
+                    Notes = x.ContactNotes,
+                    PhoneNumber = x.ContactPhoneNumber,
+                    PhoneType = x.ContactPhoneType,
 
+                }).ToList();
+            return items;
+        }
+        // Delete a contact based on a provided ID
+        public bool Delete(int id)
+        {
+            var contactToDelete = DatabaseManager.Instance.Contact.Where(x => x.ContactId == id);
+            if(contactToDelete!=null)
+            {
+                DatabaseManager.Instance.Contact.RemoveRange(contactToDelete);
+                DatabaseManager.Instance.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Update(ContactModel contactModel)
+        {
+            var dbContactModel = ToDbModel(contactModel);
+            var contactToUpdate = DatabaseManager.Instance.Contact.Find(contactModel.Id);
+            if (contactToUpdate != null)
+            {
+                DatabaseManager.Instance.Entry(contactToUpdate).CurrentValues.SetValues(dbContactModel);
+                DatabaseManager.Instance.SaveChanges();
+                return true;
+            }
+            return false;
+
+            //if(contactToUpdate != null)
+            //{
+            //    var contactToUpdate1 = DatabaseManager.Instance.Contact.Attach(contactToUpdate);
+            //    contactToUpdate1.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            //    DatabaseManager.Instance.SaveChanges();
+            //    return true;
+
+            //}
+            //return false;
+
+        }
     }
 }
